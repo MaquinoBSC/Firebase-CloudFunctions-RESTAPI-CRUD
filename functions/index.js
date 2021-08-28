2cfe3b8cde685bf5dc6ae41a0a8aaf6afaae3634
MaquinoBSC <martinare.ipn@gmail.com>
2021-08-28 14:32:48 -0500

    Configuracion de permisos para utilizar los servicios de firebase

--- functions/index.js
+++ functions/index.js
@@ -1,1 +1,15 @@
 const functions = require("firebase-functions");
+const admin= require('firebase-admin');
+
+const express= require('express');
+const app= express();
+
+//Generar permisos para usar los servicios de firebase a traves de nuestro proyecto en firebase
+// y tambien inicializamos la aplicacion de FB
+admin.initializeApp({
+    //permissions es un archivo que se descarga en firebase (configuracion del proyecto -> cuentas de servicio -> generar nueva clave privada)
+    credential: admin.credential.cert('./permissions.json'),
+    //la url la encuentramos en firebase (configuracion del proyecto -> cuentas de servicio)
+    databaseURL: "https://cloudfunctions-583e9-default-rtdb.firebaseio.com",
+});
+
